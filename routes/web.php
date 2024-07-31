1e2ca51c5fe1cb2f7e873092a00259be96358391
Randi Febriadi <randifebriadi@gmail.com>
2024-07-31 17:05:34 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Randi Febriadi",
+        "email" => "randifebriadi@gmail.com",
+        "image" => "1.jpg"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
